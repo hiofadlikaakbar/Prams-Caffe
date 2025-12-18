9493d6b3ee7af935dd005c6e96a9d75359f1805c
hiofadlikaakbar <hioakbar1@gmail.com>
2025-12-18 07:44:21 +0700

    menambah proteksi auth terhadap user yg blum login/signup

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createWebHistory, createRouter } from "vue-router";
+import { supabase } from "../lib/supabase";
 import LoginPage from "../views/LoginPage.vue";
 import SignUp from "../views/SignUp.vue";
 import MainHome from "../views/MainHome.vue";
@@ -12,26 +13,46 @@ import InvoiceView from "../views/InvoiceView.vue";
 const routes = [
   { path: "/", component: LoginPage },
   { path: "/sign-up", component: SignUp },
-  { path: "/home", component: MainHome },
-  { path: "/coffe-menu", component: CoffeMenu },
-  { path: "/milkshake-menu", component: MilkshakeMenu },
-  { path: "/snack-menu", component: SnackMenu },
-  { path: "/cart", component: Cart },
+
+  { path: "/home", component: MainHome, meta: { requiresAuth: true } },
+  { path: "/coffe-menu", component: CoffeMenu, meta: { requiresAuth: true } },
+  {
+    path: "/milkshake-menu",
+    component: MilkshakeMenu,
+    meta: { requiresAuth: true },
+  },
+  { path: "/snack-menu", component: SnackMenu, meta: { requiresAuth: true } },
+  { path: "/cart", component: Cart, meta: { requiresAuth: true } },
   {
     path: "/checkout",
     name: "checkout",
     component: CheckoutView,
+    meta: { requiresAuth: true },
   },
   {
     path: "/invoice/:orderId",
     name: "invoice",
     component: InvoiceView,
+    meta: { requiresAuth: true },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (to.meta.requiresAuth && !session) {
+    next("/");
+    return;
+  }
+
+  next();
 });
 
 export default router;
