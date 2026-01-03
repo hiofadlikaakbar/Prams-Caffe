0c3d7deb5aee251193eb9be63e97b5330eca1b3d
hiofadlikaakbar <hioakbar1@gmail.com>
2026-01-03 15:13:07 +0700

    fixing footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,12 @@
 <template>
-  <footer class="flex justify-center py-6 bg-coffe">
+  <footer class="flex justify-center items-center py-6 bg-coffe">
+    <CopyrightIcon class="w-4 h-4 text-white mr-1" />
     <p class="text-white font-lexend text-sm text-center">
-      © 2026 Prams Cafe. Semua hak dilindungi. Dibuat dengan kasih sayang
+      2025 Prams Cafe. Semua hak dilindungi. Dibuat dengan kasih sayang
     </p>
   </footer>
 </template>
+
+<script setup>
+import { CopyrightIcon } from "lucide-vue-next";
+</script>
